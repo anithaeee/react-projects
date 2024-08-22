bbefa3395ae32e4d64518bdbcdb40f0aca788002
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-08-22 19:13:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Components from './components/Components';
+import Form from './components/Form.js';
 function App() {
   return (
     <div className="App">
       <Components />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './form.css';
+import  {useState} from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
+
+const Form = () => {
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (event) =>{
+    setInputValue(event.target.value);
+  };
+  const handleClearClick = () => {
+    setInputValue('');
+  };
+ return (
+  <form>
+  <div className="forms">
+    <InputText  value={inputValue} onChange={handleInputChange}/>
+    <Button onClick={handleClearClick} />
+  </div>
+  </form>
+ )
+};
+export default Form;
